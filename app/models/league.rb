fa64da122921fe4ae4deb3c3c4528b10f837bacb
Mark Miranda <notmarkmiranda@gmail.com>
2020-05-02 21:24:42 -0600

    Add enums for membership

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,5 +1,14 @@
 class League < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: :user_id
+  has_many :memberships
 
   validates :name, uniqueness: true
+
+  after_create :create_adminship
+
+  private
+
+  def create_adminship
+    memberships.find_or_create_by(role: 1, user: owner)
+  end
 end
